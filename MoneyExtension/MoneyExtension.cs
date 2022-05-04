d11b3c0f064db01eef22eeaf7286088db2d83124
Andre Baltieri <andrebaltieri@hotmail.com>
2022-05-04 11:36:41 -0300

    Finalizado troca de nomes

--- MoneyExtension/MoneyExtension.cs
+++ MoneyExtension/MoneyExtension.cs
@@ -7,15 +7,15 @@ public static class Money
         if (amount <= 0)
             return 0;
 
-        var text = amount
+        var value = amount
             .ToString("N2")
             .Replace(",", "")
             .Replace(".", "");
         
-        if (string.IsNullOrEmpty(text))
+        if (string.IsNullOrEmpty(value))
             return 0;
 
-        int.TryParse(text, out var result);
+        int.TryParse(value, out var result);
         return result;
     }
 }
