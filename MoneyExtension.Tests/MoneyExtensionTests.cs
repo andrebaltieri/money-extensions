23382c29c4ab07280e75d6df69612c01ea40a0f8
Andre Baltieri <andrebaltieri@hotmail.com>
2022-05-04 10:29:54 -0300

    Finalizado a primeira versao

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+        
+        Assert.AreEqual(27998, cents);
+    }
+}
